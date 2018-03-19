8076f0148f00aa8af51896451c453cf897891aab
Aaron Goodrum <ag505181@gmail.com>
2018-03-18 17:30:04 -0700

    Angular Router Basics updates

--- angular/conFusion/src/app/app.module.ts
+++ angular/conFusion/src/app/app.module.ts
@@ -1,4 +1,3 @@
-import { DishService } from './services/dish.service';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
@@ -17,7 +16,9 @@ import { FooterComponent } from './footer/footer.component';
 import { AboutComponent } from './about/about.component';
 import { HomeComponent } from './home/home.component';
 import { ContactComponent } from './contact/contact.component';
+
 import { AppRoutingModule } from './app-routing/app-routing.module';
+import { DishService } from './services/dish.service';
 
 
 
