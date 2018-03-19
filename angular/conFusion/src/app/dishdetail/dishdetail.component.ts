5abc3ed9ceb6a79631c3f25dd63ac565b5ca71fb
Aaron Goodrum <ag505181@gmail.com>
2018-03-18 18:06:04 -0700

    Single Page Applications Part 2

--- angular/conFusion/src/app/dishdetail/dishdetail.component.ts
+++ angular/conFusion/src/app/dishdetail/dishdetail.component.ts
@@ -1,7 +1,13 @@
 import { MenuComponent } from './../menu/menu.component';
 
 import { Dish } from './../shared/dish';
+import { DishService } from '../services/dish.service';
+
+import { Params, ActivatedRoute } from '@angular/router';
+import { Location } from '@angular/common';
 import { DatePipe } from '@angular/common';
+
+
 
 
 import { Component, OnInit, Input } from '@angular/core';
@@ -14,12 +20,19 @@ import { Component, OnInit, Input } from '@angular/core';
 })
 export class DishdetailComponent implements OnInit {
 
-  @Input()
   dish: Dish;
 
-  constructor(private datePipe: DatePipe) { }
+  constructor(private dishservice: DishService,
+    private route: ActivatedRoute,
+    private location: Location) { }
 
   ngOnInit() {
+    let id = +this.route.snapshot.params['id'];
+    this.dish = this.dishservice.getDish(id);
+  }
+
+  goBack(): void {
+    this.location.back();
   }
 
 }
