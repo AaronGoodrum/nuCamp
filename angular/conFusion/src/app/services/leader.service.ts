1e73d14cebd4e8d1c5668172349d76fc7396260e
Aaron Goodrum <ag505181@gmail.com>
2018-03-24 19:56:54 -0700

    Simulating Time Delay within the Service

--- angular/conFusion/src/app/services/leader.service.ts
+++ angular/conFusion/src/app/services/leader.service.ts
@@ -2,20 +2,35 @@ import { Injectable } from '@angular/core';
 import { Leader } from '../shared/leader';
 import { LEADERS } from './../shared/leaders';
 
+
 @Injectable()
 export class LeaderService {
 
   constructor() { }
 
+  // -------------------------------
+  // Simulating Time Delay within the Service
+  // -------------------------------
+
   getLeaders(): Promise<Leader[]> {
-    return Promise.resolve(LEADERS);
+    return new Promise(resolve=> {
+      // Simulate server latency with 2 second delay
+        setTimeout(() => resolve(LEADERS), 2000);
+    });
+    // return Promise.resolve(LEADERS);
   }
 
   getLeader(id: number): Promise<Leader> {
-    return Promise.resolve(LEADERS.filter((leader) => (leader.id === id))[0]);
+    return new Promise(resolve => {
+      setTimeout(() => resolve(LEADERS.filter((leader) => (leader.id === id))[0]), 2000);
+    });
+    // return Promise.resolve(LEADERS.filter((leader) => (leader.id === id))[0]);
   }
 
   getFeaturedLeader(): Promise<Leader> {
-    return Promise.resolve(LEADERS.filter((leader) => leader.featured)[0]);
+    return new Promise(resolve => {
+      setTimeout(() => resolve(LEADERS.filter((leader) => leader.featured)[0]), 2000);
+    });
+    // return Promise.resolve(LEADERS.filter((leader) => leader.featured)[0]);
   }
 }
--- angular/conFusion/src/app/services/promotion.service.ts
+++ angular/conFusion/src/app/services/promotion.service.ts
@@ -6,16 +6,28 @@ import { PROMOTIONS } from '../shared/promotions';
 export class PromotionService {
 
   constructor() { }
-
+  // -------------------------------
+  // Simulating Time Delay within the Service
+  // -------------------------------
+  
   getPromotions(): Promise<Promotion[]> {
-    return Promise.resolve(PROMOTIONS);
+    return new Promise(resolve => {
+      setTimeout(() => resolve(PROMOTIONS), 2000)
+    // return Promise.resolve(PROMOTIONS);
+    });
   }
 
   getPromotion(id: number): Promise<Promotion> {
-    return Promise.resolve(PROMOTIONS.filter((promo) => (promo.id === id))[0]);
+    return new Promise(resolve => {
+      setTimeout(() => resolve(PROMOTIONS.filter((promo) => (promo.id === id))[0]), 2000)
+    // return Promise.resolve(PROMOTIONS.filter((promo) => (promo.id === id))[0]);
+    });
   }
 
   getFeaturedPromotion(): Promise<Promotion> {
-    return Promise.resolve(PROMOTIONS.filter((promotion) => promotion.featured)[0]);
+    return new Promise(resolve => {
+      setTimeout(() => resolve(PROMOTIONS.filter((promotion) => promotion.featured)[0]), 2000)
+    // return Promise.resolve(PROMOTIONS.filter((promotion) => promotion.featured)[0]);
+    });
   }
 }
