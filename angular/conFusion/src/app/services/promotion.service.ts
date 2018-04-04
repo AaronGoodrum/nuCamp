6170a314717de560b9b2c219d92b24cc74c3d9ad
Aaron Goodrum <ag505181@gmail.com>
2018-04-04 16:58:28 -0700

    task 1

--- angular/conFusion/src/app/services/promotion.service.ts
+++ angular/conFusion/src/app/services/promotion.service.ts
@@ -1,33 +1,46 @@
 import { Injectable } from '@angular/core';
+import { Http, Response } from '@angular/http';
+
 import { Promotion } from '../shared/promotion';
 import { PROMOTIONS } from '../shared/promotions';
+import { baseURL } from '../shared/baseurl';
+
+import { ProcessHttpMsgService } from './process-httpmsg.service';
+import { RestangularModule, Restangular } from 'ngx-restangular';
+
 import { Observable } from 'rxjs/Observable';
 
 import 'rxjs/add/operator/delay';
-import 'rxjs/add/observable/of';
+import 'rxjs/add/operator/map';
+import 'rxjs/add/operator/catch';
 
 @Injectable()
 export class PromotionService {
 
-  constructor() { }
-  // -------------------------------
-  // Simulating Time Delay within the Service
-  // 6. Exercise (Instructions): Angular and Promise Part 2
-  // 4. Exercise (Instructions): Angular and RxJS Part 1
-  // -------------------------------
+  constructor(private http: Http,
+    private processHTTPMsgService: ProcessHttpMsgService,
+    private restangular: Restangular) { }
 
+
+  // getPromotions(): Observable<Promotion[]> {
+  //   return Observable.of(PROMOTIONS).delay(2000);
+  // }
   getPromotions(): Observable<Promotion[]> {
-
-    return Observable.of(PROMOTIONS).delay(2000);
+    return this.restangular.all('Promotions').getList();
   }
 
+  // getPromotion(id: number): Observable<Promotion> {
+  //   return Observable.of(PROMOTIONS.filter((Promotion) => (Promotion.id === id))[0]).delay(2000);
+  // }
   getPromotion(id: number): Observable<Promotion> {
-
-    return Observable.of(PROMOTIONS.filter((Promotion) => (Promotion.id === id))[0]).delay(2000);
+    return this.restangular.one('promotions', id).get();
   }
 
+  // getFeaturedPromotion(): Observable<Promotion> {
+  //   return Observable.of(PROMOTIONS.filter((Promotion) => Promotion.featured)[0]).delay(2000);
+  // }
   getFeaturedPromotion(): Observable<Promotion> {
-
-    return Observable.of(PROMOTIONS.filter((Promotion) => Promotion.featured)[0]).delay(2000);
+    return this.restangular.all('promotions').getList({ featured: true })
+      .map(promotions => promotions[0]);
   }
 }
