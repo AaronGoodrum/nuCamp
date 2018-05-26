8829c5ab12222ad64c71e76b0fb706c88eab51d4
Aaron Goodrum <ag505181@gmail.com>
2018-05-26 15:06:29 -0700

    Node Callbacks and Error Handling

--- NodeJS/node-examples/rectangle.js
+++ NodeJS/node-examples/rectangle.js
@@ -1,3 +1,22 @@
+// Using Callbacks in Node applications
+// Error handling in Node applications
+module.exports = (x, y, callback) => {
+    if (x <= 0 || y <= 0)
+        setTimeout(() =>
+            callback(new Error("Rectangle dimensions should be greater than zero: l = " +
+                    x + ", and b = " + y),
+                null),
+            2000);
+    else
+        setTimeout(() =>
+            callback(null, {
+                perimeter: () => (2 * (x + y)),
+                area: () => (x * y)
+            }),
+            2000);
+}
+
+
 exports.perimeter = (x, y) => (2 * (x + y));
 
 exports.area = (x, y) => (x * y);
