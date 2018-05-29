faeb25e3e8b442334d8246a023a8e1ee5632ce6d
Aaron Goodrum <ag505181@gmail.com>
2018-05-29 15:48:20 -0700

    update index for routers

--- NodeJS/Assignment-week1/index.js
+++ NodeJS/Assignment-week1/index.js
@@ -1,72 +1,31 @@
 /*jshint esversion: 6 */
+const morgan = require('morgan');
 const express = require('express'),
     http = require('http');
 
 
 const hostname = 'localhost';
 const port = 3000;
+
 const bodyParser = require('body-parser');
-const morgan = require('morgan');
+
 const dishRouter = require('./routes/dishRouter');
+const promoRouter = require('./promoRouter')
+const leaderRouter = require('./leaderRouter')
 
 const app = express();
 
 app.use(morgan('dev'));
 app.use(bodyParser.json());
 
+//Getting from routes folder
 app.use('/dishes', dishRouter);
+app.use('/promotions', promoRouter)
+app.use('/leader', leaderRouter)
 
-// app.all('/dishes', (req, res, next) => {
-//     res.statusCode = 200;
-//     res.setHeader('Content-Type', 'text/plain');
-//     next();
-// });
-
-// app.get('/dishes', (req, res, next) => {
-//     res.end('Will send all the dishes to you!');
-// });
-
-// app.post('/dishes', (req, res, next) => {
-//     res.end('Will add the dish: ' + req.body.name + ' with details: ' + req.body.description);
-// });
-
-// app.put('/dishes', (req, res, next) => {
-//     res.statusCode = 403;
-//     res.end('PUT operation not supported on /dishes');
-// });
-
-// app.delete('/dishes', (req, res, next) => {
-//     res.end('Deleting all dishes');
-// });
-
-// app.get('/dishes/:dishId', (req, res, next) => {
-//     res.end('Will send details of the dish: ' + req.params.dishId + ' to you!');
-// });
-
-// app.post('/dishes/:dishId', (req, res, next) => {
-//     res.statusCode = 403;
-//     res.end('POST operation not supported on /dishes/' + req.params.dishId);
-// });
-
-// app.put('/dishes/:dishId', (req, res, next) => {
-//     res.write('Updating the dish: ' + req.params.dishId + '\n');
-//     res.end('Will update the dish: ' + req.body.name +
-//         ' with details: ' + req.body.description);
-// });
-
-// app.delete('/dishes/:dishId', (req, res, next) => {
-//     res.end('Deleting dish: ' + req.params.dishId);
-// });
 
 app.use(express.static(__dirname + '/public'));
 
-app.use((req, res, next) => {
-    console.log(req.headers);
-    res.statusCode = 200;
-    res.setHeader('Content-Type', 'text/html');
-    res.end('<html><body><h1>This is an Express Server </h1></body></html>');
-
-});
 
 const server = http.createServer(app);
 
