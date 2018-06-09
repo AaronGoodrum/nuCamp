9a7b29584d5de268baefa5c20202f8bc437787b4
Aaron Goodrum <ag505181@gmail.com>
2018-06-08 19:05:27 -0700

    leader router

--- NodeJS/conFusionServer/routes/leaderRouter.js
+++ NodeJS/conFusionServer/routes/leaderRouter.js
@@ -1,52 +1,220 @@
 var express = require('express');
 var bodyParser = require('body-parser');
 
+const Leaders = require('../models/leadership');
+
 var leaderRouter = express.Router();
 
 leaderRouter.use(bodyParser.json());
 
 leaderRouter.route('/')
-    .all(function (req, res, next) {
-        res.writeHead(200, {
-            'Content-Type': 'text/plain'
-        });
-        next();
-    })
-
-    .get(function (req, res, next) {
-        res.end('Will send all the leaders to you!');
-    })
-
-    .post(function (req, res, next) {
-        res.end('Will add the leader: ' + req.body.name + ' with details: ' + req.body.description);
-    })
-
-    .delete(function (req, res, next) {
-        res.end('Deleting all leaders');
-    });
+	.get((req, res, next) => {
+		Leaders.find({})
+			.then((leaderes) => {
+				res.statusCode = 200;
+				res.setHeader('Content-Type', 'application/json');
+				res.json(leaderes);
+			}, (err) => next(err))
+			.catch((err) => next(err));
+	})
+	.post((req, res, next) => {
+		Leaders.create(req.body)
+			.then((leader) => {
+				console.log('Dish Created ', leader);
+				res.statusCode = 200;
+				res.setHeader('Content-Type', 'application/json');
+				res.json(leader);
+			}, (err) => next(err))
+			.catch((err) => next(err));
+	})
+	.put((req, res, next) => {
+		res.statusCode = 403;
+		res.end('PUT operation not supported on /leaderes');
+	})
+	.delete((req, res, next) => {
+		Leaders.remove({})
+			.then((resp) => {
+				res.statusCode = 200;
+				res.setHeader('Content-Type', 'application/json');
+				res.json(resp);
+			}, (err) => next(err))
+			.catch((err) => next(err));
+	});
 
 leaderRouter.route('/:leaderId')
-    .all(function (req, res, next) {
-        res.writeHead(200, {
-            'Content-Type': 'text/plain'
-        });
-        next();
-    })
+	.get((req, res, next) => {
+		Leaders.findById(req.params.leaderId)
+			.then((leader) => {
+				res.statusCode = 200;
+				res.setHeader('Content-Type', 'application/json');
+				res.json(leader);
+			}, (err) => next(err))
+			.catch((err) => next(err));
+	})
+	.post((req, res, next) => {
+		res.statusCode = 403;
+		res.end('POST operation not supported on /leaderes/' + req.params.leaderId);
+	})
+	.put((req, res, next) => {
+		Leaders.findByIdAndUpdate(req.params.leaderId, {
+				$set: req.body
+			}, {
+				new: true
+			})
+			.then((leader) => {
+				res.statusCode = 200;
+				res.setHeader('Content-Type', 'application/json');
+				res.json(leader);
+			}, (err) => next(err))
+			.catch((err) => next(err));
+	})
+	.delete((req, res, next) => {
+		Leaders.findByIdAndRemove(req.params.leaderId)
+			.then((resp) => {
+				res.statusCode = 200;
+				res.setHeader('Content-Type', 'application/json');
+				res.json(resp);
+			}, (err) => next(err))
+			.catch((err) => next(err));
+	});
 
-    .get(function (req, res, next) {
-        res.end('Will send details of the leader: ' + req.params.leaderId + ' to you!');
-    })
+leaderRouter.route('/:leaderId/comments')
+	.get((req, res, next) => {
+		Leaders.findById(req.params.leaderId)
+			.then((leader) => {
+				if (leader != null) {
+					res.statusCode = 200;
+					res.setHeader('Content-Type', 'application/json');
+					res.json(leader.comments);
+				} else {
+					err = new Error('Leader ' + req.params.leaderId + ' not found');
+					err.status = 404;
+					return next(err);
+				}
+			}, (err) => next(err))
+			.catch((err) => next(err));
+	})
+	.post((req, res, next) => {
+		Leaders.findById(req.params.leaderId)
+			.then((leader) => {
+				if (leader != null) {
+					leader.comments.push(req.body);
+					leader.save()
+						.then((leader) => {
+							res.statusCode = 200;
+							res.setHeader('Content-Type', 'application/json');
+							res.json(leader);
+						}, (err) => next(err));
+				} else {
+					err = new Error('Leader ' + req.params.leaderId + ' not found');
+					err.status = 404;
+					return next(err);
+				}
+			}, (err) => next(err))
+			.catch((err) => next(err));
+	})
+	.put((req, res, next) => {
+		res.statusCode = 403;
+		res.end('PUT operation not supported on /leaderes/' +
+			req.params.leaderId + '/comments');
+	})
+	.delete((req, res, next) => {
+		Leaders.findById(req.params.leaderId)
+			.then((leader) => {
+				if (leader != null) {
+					for (var i = (leader.comments.length - 1); i >= 0; i--) {
+						leader.comments.id(leader.comments[i]._id).remove();
+					}
+					leader.save()
+						.then((leader) => {
+							res.statusCode = 200;
+							res.setHeader('Content-Type', 'application/json');
+							res.json(leader);
+						}, (err) => next(err));
+				} else {
+					err = new Error('Leader ' + req.params.leaderId + ' not found');
+					err.status = 404;
+					return next(err);
+				}
+			}, (err) => next(err))
+			.catch((err) => next(err));
+	});
 
-    .put(function (req, res, next) {
-        // Json .put 
-        // { "name": "xTESTx", "description": "xTESTx" }
-        res.write('Updating the leader: ' + req.params.leaderId + '\n');
-        res.end('Will update the leader: ' + req.body.name +
-            ' with details: ' + req.body.description);
-    })
-
-    .delete(function (req, res, next) {
-        res.end('Deleting leader: ' + req.params.leaderId);
-    });
+leaderRouter.route('/:leaderId/comments/:commentId')
+	.get((req, res, next) => {
+		Leaders.findById(req.params.leaderId)
+			.then((leader) => {
+				if (leader != null && leader.comments.id(req.params.commentId) != null) {
+					res.statusCode = 200;
+					res.setHeader('Content-Type', 'application/json');
+					res.json(leader.comments.id(req.params.commentId));
+				} else if (leader == null) {
+					err = new Error('Leader ' + req.params.leaderId + ' not found');
+					err.status = 404;
+					return next(err);
+				} else {
+					err = new Error('Comment ' + req.params.commentId + ' not found');
+					err.status = 404;
+					return next(err);
+				}
+			}, (err) => next(err))
+			.catch((err) => next(err));
+	})
+	.post((req, res, next) => {
+		res.statusCode = 403;
+		res.end('POST operation not supported on /leaderes/' + req.params.leaderId +
+			'/comments/' + req.params.commentId);
+	})
+	.put((req, res, next) => {
+		Leaders.findById(req.params.leaderId)
+			.then((leader) => {
+				if (leader != null && leader.comments.id(req.params.commentId) != null) {
+					if (req.body.rating) {
+						leader.comments.id(req.params.commentId).rating = req.body.rating;
+					}
+					if (req.body.comment) {
+						leader.comments.id(req.params.commentId).comment = req.body.comment;
+					}
+					leader.save()
+						.then((leader) => {
+							res.statusCode = 200;
+							res.setHeader('Content-Type', 'application/json');
+							res.json(leader);
+						}, (err) => next(err));
+				} else if (leader == null) {
+					err = new Error('Leader ' + req.params.leaderId + ' not found');
+					err.status = 404;
+					return next(err);
+				} else {
+					err = new Error('Comment ' + req.params.commentId + ' not found');
+					err.status = 404;
+					return next(err);
+				}
+			}, (err) => next(err))
+			.catch((err) => next(err));
+	})
+	.delete((req, res, next) => {
+		Leaders.findById(req.params.leaderId)
+			.then((leader) => {
+				if (leader != null && leader.comments.id(req.params.commentId) != null) {
+					leader.comments.id(req.params.commentId).remove();
+					leader.save()
+						.then((leader) => {
+							res.statusCode = 200;
+							res.setHeader('Content-Type', 'application/json');
+							res.json(leader);
+						}, (err) => next(err));
+				} else if (leader == null) {
+					err = new Error('Leader ' + req.params.leaderId + ' not found');
+					err.status = 404;
+					return next(err);
+				} else {
+					err = new Error('Comment ' + req.params.commentId + ' not found');
+					err.status = 404;
+					return next(err);
+				}
+			}, (err) => next(err))
+			.catch((err) => next(err));
+	});
 
 module.exports = leaderRouter;
